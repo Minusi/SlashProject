efb7418fad76551f552353075ddf02d6ad037af9
wogud <wogudac@naver.com>
2016-11-09 16:04:54 +0900

    161109

--- Slash/Assets/Scripts/Game Scene/Player.cs
+++ Slash/Assets/Scripts/Game Scene/Player.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
+    static public int Speed = 7;
     bool hasArmor;
     int bulletCount;
     Item item;
@@ -11,30 +13,71 @@ public class Player : MonoBehaviour {
 
     void Attack()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+        }
+
     }
 
     void Hit()
     {
+        if (hasArmor == true)
+        {
+            hasArmor = false;
+        }
+        else
+        {
+            Die();
+        }
     }
 
-    void Die()
+    void Die()  
     {
+        SceneManager.LoadScene("Game Scene");
+        // 버튼 restart, menu버튼 띄우기
     }
 
     void Fire()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
     }
 
     void Avoid()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // 회피
+        }
     }
 
     void ActiveItem()
     {
+        if (Input.GetKey(KeyCode.E))
+        {
+            // 아이템 사용
+        }
     }
 
-    void Move(Vector2 dir)
+    void Move()
     {
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Translate(new Vector2(-1 * Time.deltaTime * Speed, 0));
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Translate(new Vector2(1 * Time.deltaTime * Speed, 0));
+        if (Input.GetKey(KeyCode.W))
+            this.transform.Translate(new Vector2(0, 1 * Time.deltaTime * Speed));
+        if (Input.GetKey(KeyCode.S))
+            this.transform.Translate(new Vector2(0, -1 * Time.deltaTime * Speed));
     }
+  
+    
+    void Update()
+    {
+        Move();
 
+    }
 }
--- Slash/Assets/Scripts/InputManager.cs
+++ Slash/Assets/Scripts/InputManager.cs
@@ -10,10 +10,14 @@ public class InputManager : MonoBehaviour {
     public static event Action<Vector2> OnFire;
     public static event Action<Vector2> OnActiveItem;
 
+
+    
     KeyCode keyCode_moveRight;
     KeyCode keyCode_moveLeft;
     KeyCode keyCode_moveUp;
     KeyCode keyCode_moveDown;
     KeyCode keyCode_activeItem;
     KeyCode keyCode_avoid;
+    
+
 }
