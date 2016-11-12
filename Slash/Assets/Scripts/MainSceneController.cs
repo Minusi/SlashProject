72d6b97cefe4f84637bf83cd833deaff38e6f76d
wogud <wogudac@naver.com>
2016-11-12 23:49:30 +0900

    161112

--- Slash/Assets/Scripts/MainSceneController.cs
+++ Slash/Assets/Scripts/MainSceneController.cs
@@ -1,22 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.SceneManagement;
 
-public class MainSceneController
+public class MainSceneController : MonoBehaviour
 {
+    public GameObject AchievementTab;
+    public GameObject RankingTab;
+    public GameObject[] Btn;
+    public GameObject BackBtn;
 
     public void LoadGameScene()
     {
-    }
-
-    public void FloatAchievementTab()
-    {
+        SceneManager.LoadScene("Game Scene");
     }
 
     public void FloatRankingTab()
     {
+        RankingTab.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        for (int i = 0; i < Btn.Length; i++)
+        {
+            Btn[i].SetActive(false);
+        }
+        BackBtn.SetActive(true);
     }
 
+    public void FloatAchievementTab()
+    {
+        AchievementTab.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        for(int i=0;i<Btn.Length;i++)
+        {
+            Btn[i].SetActive(false);
+        }
+        BackBtn.SetActive(true);
+    }
 
-    public void Exit() { }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        AchievementTab.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        RankingTab.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        for (int i = 0; i < Btn.Length; i++)
+        {
+            Btn[i].SetActive(true);
+        }
+        BackBtn.SetActive(false);
+    }
 }
