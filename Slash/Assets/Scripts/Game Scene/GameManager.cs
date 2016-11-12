3a15fc1abb4dd9fec90f68bf607faa9d4362c10a
wogud <wogudac@naver.com>
2016-11-12 23:52:06 +0900

    161112

--- Slash/Assets/Scripts/Game Scene/GameManager.cs
+++ Slash/Assets/Scripts/Game Scene/GameManager.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    public Text scoretxt;
 
     public static GameManager GetInstance()
     {
@@ -11,13 +14,28 @@ public class GameManager : MonoBehaviour {
     public Stage[] stages;
 
     int stageLevel;
-    int score;
+    public float fscore = 10000;
 
     MiniMapGenerator minimapGenerator;
     MiniMap map;
 
     void LoadStage()
     {
+        if (stageLevel == 1)
+        {
+            // 스테이지 1일때
+        }
     }
 
+    void ScoreDisplay()
+    {
+        int iscore = (int)fscore;
+        scoretxt.text = iscore.ToString();
+        fscore -= 0.1f;
+    }
+
+    void Update()
+    {
+        ScoreDisplay();
+    }
 }
--- Slash/Assets/Scripts/Tab.cs
+++ Slash/Assets/Scripts/Tab.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tab {
+public class Tab {      // 안씀
     void Exit()
     {
+        //MainSceneController.AchievementTab.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        //MainSceneController.RankingTab.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        //for(int i=0; i < MainSceneController.Btn.Length; i++)
+        //{
+        //    MainSceneController.Btn[i].SetActive(true);
+        //}
     }
 }
